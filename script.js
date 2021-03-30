ed154e1a6384f8c454c1949241595c54db3a7006
shardul <shardul.aeer@somaiya.edu>
2021-03-30 20:56:49 +0530

    feat: Display sett stake data for DIGG

--- script.js
+++ script.js
@@ -25,4 +25,51 @@ async function drawChart () {
     chart.draw(data, options)
 }
 
+function getTable(rows) {
+    return `<table class="table table-image">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Sett</th>
+                        <th scope="col">DIGG</th>
+                        <th scope="col">Value (USD)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${rows}
+                </tbody>
+            </table>`
+}
 // Get sett data
+async function getSettData() {
+    const settTable = document.getElementById('sett-table')
+    const setts = ['DIGG', 'DIGG-WBTC', 'SLP-DIGG-WBTC']
+    const resp = await fetch('https://api.badger.finance/v2/geysers?chain=eth')
+    const geysers = await resp.json()
+    console.log('Body', geysers)
+
+
+
+    const result = geysers
+        .filter((geyser) => setts.includes(geyser.asset))
+        .map(geyser => {
+            const tokenData = geyser.tokens.filter(data => {
+                return data.name === 'Digg'
+            })[0]
+            return `<tr>
+                        <td><img src="https://app.badger.finance/assets/icons/${geyser.asset.toLowerCase()}.png" class="img-fluid" width="40" height="40"></td>
+                        <td>${geyser.name}</td>
+                        <td>${tokenData.balance.toFixed(2)}</td>
+                        <td>${tokenData.value.toFixed(2)}</td>
+                    </tr>`
+        }).reduce((previous, current) => {
+            return previous += current
+        })
+
+    console.log('Result', result)
+
+    settTable.innerHTML = getTable(result)
+
+}
+
+window.onload(getSettData())
