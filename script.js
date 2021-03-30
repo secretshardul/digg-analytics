9a78d739dc131fa67411bef8a9161466ab5d85c5
shardul <shardul.aeer@somaiya.edu>
2021-03-30 18:57:34 +0530

    style: Column layout for pie chart and sett list

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function drawChart () {
     ])
 
     // Optional; add a title and set the width and height of the chart
-    var options = { 'title': 'DIGG circulation', 'width': 550, 'height': 400 }
+    var options = { 'title': 'DIGG circulation', 'height': 400 }
 
     // Display the chart inside the <div> element with id="piechart"
     var chart = new google.visualization.PieChart(document.getElementById('piechart'))
