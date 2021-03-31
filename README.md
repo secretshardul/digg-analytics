b123b2b649666c4de7a5af4c66972ce792d71f85
shardul <shardul.aeer@somaiya.edu>
2021-03-31 09:40:34 +0530

    feat: Display ownership pie chart

--- README.md
+++ README.md
@@ -9,12 +9,77 @@
 - Token data: total supply, supply pie chart (vesting funds, rewards escrow, DAO funds, Airdrop, circulation). Get from CoinGecko API
     - Total and circulating supply: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=digg
     - Circulation breakdown: Subtract these from total supply to get circulating supply
-        1. Team vesting funds: https://etherscan.io/token/0x798d1be841a82a273720ce31c822c61a67a601c3?a=0x124FD4A9bd4914b32c77C9AE51819b1181dbb3D4
+        1. Team vesting funds: https://etherscan.io/token/0x798d1be841a82a273720ce31c822c61a67a601c3?a=0x124fd4a9bd4914b32c77c9ae51819b1181dbb3d4
         2. Rewards escrow: https://etherscan.io/token/0x798d1be841a82a273720ce31c822c61a67a601c3?a=0x19d099670a21bc0a8211a89b84cedf59abb4377f
-        3. DAO funds: https://etherscan.io/token/0x798d1be841a82a273720ce31c822c61a67a601c3?a=0x5A54Ca44e8F5A1A695f8621f15Bfa159a140bB61
+        3. DAO funds: https://etherscan.io/token/0x798d1be841a82a273720ce31c822c61a67a601c3?a=0x5a54ca44e8f5a1a695f8621f15bfa159a140bb61
         4. Airdrop: https://etherscan.io/token/0x798d1be841a82a273720ce31c822c61a67a601c3?a=0x5e79958efbb8afdedb6ec7107110f329e4eafffa (It's 0 now)
 
             ```sh
             curl 'https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x798d1be841a82a273720ce31c822c61a67a601c3&address=0x5e79958efbb8afdedb6ec7107110f329e4eafffa&tag=latest&apikey=S6BUBDEGTTTYHTQT3UN1Y6SAJ4F8UVFHZW'
             ```
 - Google search interest: Not possible
+
+## Subgraph usage
+```graphql
+{
+  tokenBalances(orderBy: balance, orderDirection: desc) {
+    id
+    balance
+    token {
+      name
+    }
+  }
+}
+```
+
+```json
+{
+  "data": {
+    "tokenBalances": [
+      {
+        "balance": "46316835694926478169428394003475163141307993866256225615783033601600000000000",
+        "id": "0x5a54ca44e8f5a1a695f8621f15bfa159a140bb61-0x798d1be841a82a273720ce31c822c61a67a601c3",
+        "token": {
+          "name": "Digg"
+        }
+      },
+      {
+        "balance": "20173219154436926608344814587747762963314633070231619877797290934340870482621",
+        "id": "0x4a8651f2edd68850b944ad93f2c67af817f39f62-0x798d1be841a82a273720ce31c822c61a67a601c3",
+        "token": {
+          "name": "Digg"
+        }
+      },
+```
+
+ID Format in `0x5a54ca44e8f5a1a695f8621f15bfa159a140bb61-0x798d1be841a82a273720ce31c822c61a67a601c3`
+1. First address is holder address (DAO funds)
+2. Last address is DIGG contract address
+
+Must be in lowercase
+
+Find DIGG balance
+```
+{
+  tokenBalance(id: "0x124fd4a9bd4914b32c77c9ae51819b1181dbb3d4-0x798d1be841a82a273720ce31c822c61a67a601c3") {
+    id
+    balance
+    token {
+      name
+    }
+  }
+}
+```
+
+BigInt issue
+- https://thegraph.com/docs/assemblyscript-api#built-in-types
+- https://github.com/graphprotocol/graph-ts : Can have decoder
+- https://stackoverflow.com/questions/59317194/how-to-use-npm-package-as-normal-javascript-in-html
+
+Call GraphQL
+```
+curl -X POST \
+-H "Content-Type: application/json" \
+-d '{"query": "{ hello }"}' \
+https://api.thegraph.com/subgraphs/name/darruma/badger
+```
--- script.js
+++ script.js
@@ -2,23 +2,84 @@
 google.charts.load('current', { 'packages': ['corechart'] })
 google.charts.setOnLoadCallback(drawChart)
 
-// Get circulation data
-async function getCirculation() {
+async function getTotalSupply() {
+    const resp = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=digg')
+        .then(result => result.json())
+    console.log('Digg stats', resp)
+
+    const supply = resp[0].total_supply
+    console.log('Supply', supply)
+    return supply
+}
+
+async function getBalance(address) {
+    const sharesPerFragment = 43880509104374636998557764964276047643928934163648035762810023405
+    const DIGG_CONTRACT_ADDR = '0x798d1be841a82a273720ce31c822c61a67a601c3'
+
+    const query = `query getBalance($id: ID!) {
+        tokenBalance(id: $id) {
+            id
+            balance
+            token {
+            name
+            }
+        }
+    }`
+
+    const graphResp = await fetch("https://api.thegraph.com/subgraphs/name/darruma/badger", {
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({
+            query,
+            variables: {
+                id: address + '-' + DIGG_CONTRACT_ADDR
+            }
+        }),
+        method: 'POST',
+    });
+    const result = await graphResp.json()
+    const shares = result.data.tokenBalance.balance
+
+    const balance = shares / (sharesPerFragment * 10**9)
+    console.log('Got balance', balance)
+    return balance
 
 }
+
 // Draw the chart and set the chart values
 async function drawChart () {
+
+    const majorDiggHolders = {
+        'Team vesting funds': '0x124fd4a9bd4914b32c77c9ae51819b1181dbb3d4',
+        'Rewards escrow': '0x19d099670a21bc0a8211a89b84cedf59abb4377f',
+        'DAO funds': '0x5a54ca44e8f5a1a695f8621f15bfa159a140bb61'
+    }
+
+    const supply = await getTotalSupply()
+
+    const bal1 = await getBalance(majorDiggHolders['Team vesting funds'])
+    const bal2 = await getBalance(majorDiggHolders['Rewards escrow'])
+    const bal3 = await getBalance(majorDiggHolders['DAO funds'])
+
+    const other = supply - (bal1 + bal2 + bal3)
+
+
     var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Work', 8],
-        ['Eat', 2],
-        ['TV', 4],
-        ['Gym', 2],
-        ['Sleep', 8]
+        ['Ownership', 'DIGG tokens'],
+        ['Team vesting funds', bal1],
+        ['Rewards escrow', bal2],
+        ['DAO funds', bal3],
+        ['Other', other]
     ])
 
     // Optional; add a title and set the width and height of the chart
-    var options = { 'height': 300 }
+    var options = {
+        chartArea: { width: '100%', height: '100%' },
+        // 'height': 400,
+        // 'width': 700,
+        pieSliceText: 'value-and-percentage'
+     }
 
     // Display the chart inside the <div> element with id="piechart"
     var chart = new google.visualization.PieChart(document.getElementById('piechart'))
@@ -42,13 +103,12 @@ function getTable(rows) {
 }
 // Get sett data
 async function getSettData() {
+    console.log('Getting sett data')
     const settTable = document.getElementById('sett-table')
     const setts = ['DIGG', 'DIGG-WBTC', 'SLP-DIGG-WBTC']
     const resp = await fetch('https://api.badger.finance/v2/geysers?chain=eth')
     const geysers = await resp.json()
     console.log('Body', geysers)
-
-
 
     const result = geysers
         .filter((geyser) => setts.includes(geyser.asset))
@@ -71,5 +131,3 @@ async function getSettData() {
     settTable.innerHTML = getTable(result)
 
 }
-
-window.onload(getSettData())
